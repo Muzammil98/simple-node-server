93173bf521681f0520ba9dc14de477abc7c7e003
Muzammil98 <muzammilsarwar0@gmail.com>
2021-10-30 12:38:21 +0500

    Renamed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('Response from Generic backend server, Now updated');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, (error) => {
     if (error) {
         return console.log('Error: ' + err);
